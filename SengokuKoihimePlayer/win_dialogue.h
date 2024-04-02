ae98667292412ff6c44e36e81b06db82d68a544c
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2024-04-03 08:43:07 +0900

    Add utility

--- SengokuKoihimePlayer/win_dialogue.h
+++ SengokuKoihimePlayer/win_dialogue.h
@@ -1,10 +1,12 @@
-#ifndef WIN_DIALOGUE_H_
+﻿#ifndef WIN_DIALOGUE_H_
 #define WIN_DIALOGUE_H_
 
 #include <string>
 
 namespace win_dialogue
 {
-	std::wstring SelectWorkFolder();
+	std::wstring SelectWorkFolder(void *hParentWnd);
+	std::wstring SelectOpenFile(const wchar_t* pwzFileType, const wchar_t* pwzSpec, void* hParentWnd);
+	void ShowMessageBox(const char* pzTitle, const char* pzMessage);
 }
 #endif // WIN_DIALOGUE_H_
