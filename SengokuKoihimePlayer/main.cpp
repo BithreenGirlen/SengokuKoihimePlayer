31cf8031544cb08f4aaa07410e7443ffa299425c
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2025-01-29 16:26:35 +0900

    Delete default files.

---
+++ SengokuKoihimePlayer/main.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include "framework.h"
+#include "main_window.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    int iRet = 0;
+    CMainWindow sMainWindow;
+    bool bRet = sMainWindow.Create(hInstance, L"SengokuKoihime player");
+    if (bRet)
+    {
+        ::ShowWindow(sMainWindow.GetHwnd(), nCmdShow);
+        iRet = sMainWindow.MessageLoop();
+    }
+
+    return iRet;
+}
